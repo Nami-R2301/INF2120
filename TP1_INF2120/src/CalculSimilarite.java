4e9c549876c871c1fd7b28a8299d03bbca3d839a
Nami Reghbati (REGN03079808) <je191030@ens.uqam.ca>
2020-02-06 18:41:35 -0500

    Photos et diagrams pour le TP1

--- TP1_INF2120/src/CalculSimilarite.java
+++
@@ -1,6 +0,0 @@
-public class CalculSimilarite extends SommeDeviationMin {
-
-    public CalculSimilarite(String arnA, String arnB, int distanceMax) {
-        super(arnA, arnB, distanceMax);
-    }
-}
--- TP1_INF2120/src/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Resultat s1 = new DeviationMin("", "", 3);
-        System.out.println(s1.toString());
-    }
-}
